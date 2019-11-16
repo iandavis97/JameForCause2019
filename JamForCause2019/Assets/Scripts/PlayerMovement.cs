5e6bbd5089425a23e3e847398d4ccdb59466b5d5
Ian Davis <iandavis97@gmail.com>
2019-11-15 20:54:28 -0500

    player jumping

--- JamForCause2019/Assets/Scripts/PlayerMovement.cs
+++ JamForCause2019/Assets/Scripts/PlayerMovement.cs
@@ -7,12 +7,8 @@ public class PlayerMovement : MonoBehaviour
     
     public float speed;
     private Rigidbody2D rb;
-    /*
-    private Vector2 moveVelocity;
     private bool isGrounded;
     public float jumpForce;
-    public Vector2 jump;
-    */
     // Start is called before the first frame update
     void Start()
     {
@@ -38,13 +34,31 @@ public class PlayerMovement : MonoBehaviour
          * every frame, subtract .25 * deltatime from y velocity until it hits ground
          */
         /*
-        Vector2 moveInput = new Vector2(moveInputX, 0f);
-        moveVelocity = moveInput.normalized * speed;
         */
+        if(isGrounded==true&&Input.GetKeyDown(KeyCode.Space))
+        {
+            isGrounded = false;
+            rb.AddForce(new Vector2(0, jumpForce));
+        }
         HandleMovement(horizontal);
     }
-    private bool isGrounded()
+    /*
+    private bool IsGrounded()
     {
-
+        if (rb.velocity.y <= 0)
+        {
+            return true;
+        }
+        else
+            return false;
+    }*/
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Ground") // GameObject is a type, gameObject is the property
+        {
+            isGrounded = true;
+        }
+        else
+            isGrounded = false;
     }
 }
