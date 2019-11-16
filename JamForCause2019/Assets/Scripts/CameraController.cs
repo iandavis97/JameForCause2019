68cc0aee30081da12a18834d41bc17d9e97611b4
Ian Davis <iandavis97@gmail.com>
2019-11-16 13:15:55 -0500

    updating checkpoint prefab

--- JamForCause2019/Assets/Scripts/CameraController.cs
+++ JamForCause2019/Assets/Scripts/CameraController.cs
@@ -37,5 +37,8 @@ public class CameraController : MonoBehaviour
             targetPos = transform.position = transform.position + (targetDirection.normalized * interpVelocity * Time.deltaTime);
             transform.position = Vector3.Lerp(transform.position, targetPos + offset, .25f);
         }*/
+        float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
+        float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocity.x, smoothTimeY);
+        transform.position = new Vector3(posX, posY, transform.position.z);
     }
 }
