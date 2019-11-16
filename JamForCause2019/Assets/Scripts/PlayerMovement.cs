a3beb32a64e72b06f0c2802ef8240f0a23520475
Ian Davis <iandavis97@gmail.com>
2019-11-15 20:30:56 -0500

    changing folder structure

--- JamForCause2019/Assets/Scripts/PlayerMovement.cs
+++ JamForCause2019/Assets/Scripts/PlayerMovement.cs
@@ -4,42 +4,47 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    
     public float speed;
     private Rigidbody2D rb;
+    /*
     private Vector2 moveVelocity;
     private bool isGrounded;
     public float jumpForce;
     public Vector2 jump;
-
+    */
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        jump = new Vector2(0f, 2.0f);
+    }
+
+    private void HandleMovement(float horizontal)
+    {
+        rb.velocity = new Vector2(horizontal*speed,rb.velocity.y);
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        //moving horizontally
-        float moveInputX = Input.GetAxisRaw("Horizontal");
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-
-            rb.AddForce(jump * jumpForce, ForceMode2D.Impulse);
-            isGrounded = false;
-        }
-        Vector2 moveInput = new Vector2(moveInputX, 0f);
-        moveVelocity = moveInput.normalized * speed;
-    }
-
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + moveVelocity * Time.deltaTime);
-    }
+        //moving horizontally
+        float horizontal = Input.GetAxisRaw("Horizontal");
 
-    void OnCollisionStay()
+        /*
+         * if up arrow pressed single frame
+         * 
+         * set y velocity to some high value
+         * 
+         * every frame, subtract .25 * deltatime from y velocity until it hits ground
+         */
+        /*
+        Vector2 moveInput = new Vector2(moveInputX, 0f);
+        moveVelocity = moveInput.normalized * speed;
+        */
+        HandleMovement(horizontal);
+    }
+    private bool isGrounded()
     {
-        isGrounded = true;
+
     }
 }
