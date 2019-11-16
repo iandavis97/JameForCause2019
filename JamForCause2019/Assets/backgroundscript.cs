81ce1f4a7333569f12f4a08009902651a42679f5
Andy Tiffin <awt1910@g.rit.edu>
2019-11-16 17:14:09 -0500

    level finished

---
+++ JamForCause2019/Assets/backgroundscript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backgroundscript : MonoBehaviour
+{
+    public GameObject cameraobj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(cameraobj.transform.position.x, cameraobj.transform.position.y, 3);
+    }
+}
