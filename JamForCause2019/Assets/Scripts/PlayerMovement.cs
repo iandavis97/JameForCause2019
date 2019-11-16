596b4e8f00a4e899d6fd427375eec5a37ce4c270
Ian Davis <iandavis97@gmail.com>
2019-11-15 19:46:27 -0500

    basic player movement begun

---
+++ JamForCause2019/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private RigidBody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
