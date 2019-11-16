600c79b0e8f5332b3bf52af4efc9938750be29ca
Ian Davis <iandavis97@gmail.com>
2019-11-16 13:03:59 -0500

    updating player prefab

--- JamForCause2019/Assets/Scripts/CameraController.cs
+++ JamForCause2019/Assets/Scripts/CameraController.cs
@@ -4,22 +4,29 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    /*
     public float interpVelocity;
     public float minDistance;
     public float followDistance;
     public GameObject target;
     public Vector3 offset;
-    Vector3 targetPos;
+    Vector3 targetPos;*/
+    private Vector2 velocity;
+    public float smoothTimeY;
+    public float smoothTimeX;
+    public GameObject player;
 
     // Start is called before the first frame update
     void Start()
     {
-        targetPos = transform.position;
+        // targetPos = transform.position;
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        /*
         if (target)
         {
             Vector3 posNoZ = transform.position;
@@ -29,6 +36,6 @@ public class CameraController : MonoBehaviour
             interpVelocity = targetDirection.magnitude * 5f;
             targetPos = transform.position = transform.position + (targetDirection.normalized * interpVelocity * Time.deltaTime);
             transform.position = Vector3.Lerp(transform.position, targetPos + offset, .25f);
-        }
+        }*/
     }
 }
